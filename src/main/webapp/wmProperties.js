966d1394643c0b76e100d8bbe2d1b669bc63d4a4
QmLbNkUbWQ <automate1@wavemaker.com>
2017-09-12 20:48:29 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_MzIvyVWWee",
+  "homePage" : "Main",
+  "name" : "Auto_MzIvyVWWee",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
